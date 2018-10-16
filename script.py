285dafabb66d6fbf2ac76a8620e6a22a66f49d20
Goncalo <goncalo.s.and@gmail.com>
2018-10-16 20:08:12 +0100

    add different sizes

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-import pygame
-import random
+import pygame as pg
+from random import randint
+vec = pg.math.Vector2
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -15,19 +16,20 @@ GAME_UNIT = 30
 # Screen
 WIDTH = 9
 HEIGHT = 10
-FPS = 10
+FPS = 60
 
 # Blocks:
 VELX = GAME_UNIT
 VELY = GAME_UNIT
 TIME_TO_MOVE = 1000
+TIME_TO_PRESS = 150
 
 
 ############################################################
 # Margins:
 def create_margin(pos, sizes):
-    margin = pygame.sprite.Sprite()
-    margin.image = pygame.Surface(sizes)
+    margin = pg.sprite.Sprite()
+    margin.image = pg.Surface(sizes)
     margin.image.fill(BLUE)
     margin.rect = margin.image.get_rect()
     margin.rect.topleft = pos
@@ -50,85 +52,44 @@ def create_margins():
 
 ############################################################
 # Blocks:
-def create_side(block, which_side):
-    # settings:
-    side_size = 5
-    if which_side in ['left', 'right']:
-        width = side_size
-        height = GAME_UNIT + 1
-        y = - GAME_UNIT - 1
-        if which_side == 'left':
-            x = block.x - 1
-        elif which_side == 'right':
-            x = block.x + GAME_UNIT - side_size + 1
-            print width, height, x, y
-    elif which_side == 'bottom':
-        width = GAME_UNIT - 2 * side_size + 2
-        height = side_size
-        x = block.x + side_size - 1
-        y = - side_size
-    # logic:
-    side = pygame.sprite.Sprite()
-    side.image = pygame.Surface((width, height))
-    side.image.fill(RED)
-    side.rect = side.image.get_rect()
-    side.rect.x = x
-    side.rect.y = y
-    side.dx = block.dx
-    side.dy = block.dy
-    return block, side
 
-
-def create_new_block():
-    block = pygame.sprite.Group()
-    block.x = (WIDTH / 2) * GAME_UNIT
-    block.y = 0
-    block.dx = 0
-    block.dy = VELY
+def create_new_block(all_sprites):
+    print 'create_new_block'
+    block = pg.sprite.Sprite()
+    block.pos = vec((WIDTH / 2) * GAME_UNIT, 0)
+    block.vel = vec(0, 0)
+    block.image = pg.Surface((GAME_UNIT, randint(2, 4) * GAME_UNIT))
+    block.image.fill(GREEN)
+    block.rect = block.image.get_rect()
+    block.rect.x = (WIDTH / 2) * GAME_UNIT
+    block.stop = False
     block.can_move_left = True
     block.can_move_right = True
 
-    # create sides:
-    sides = pygame.sprite.Group()
-    block, side_left = create_side(block, 'left')
-    block, side_right = create_side(block, 'right')
-    block, side_bottom = create_side(block, 'bottom')
-    sides.add(side_left)
-    sides.add(side_right)
-    sides.add(side_bottom)
-    block.add(sides)
-    # all_sprites.add(block)
+    block.update_time = pg.time.get_ticks()
+    block.time_to_press = pg.time.get_ticks()
+    all_sprites.add(block)
 
-    # block = pygame.sprite.Sprite()
-    # block.image = pygame.Surface((GAME_UNIT, GAME_UNIT))
-    # block.image.fill(GREEN)
-    # block.rect = block.image.get_rect()
-    # block.rect.x = (WIDTH / 2) * GAME_UNIT
-
-    # for unit in block:
-    #     unit.x = block.x
-    #     unit.y = block.y
-    #     unit.dx = block.dx
-    #     unit.dy = block.dy
-    block.update_time = pygame.time.get_ticks()
-
-    return block, sides, side_left, side_right, side_bottom
-
-
-def events(event, block):
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_LEFT and block.can_move_left:
-            block.dx = -VELX
-        if event.key == pygame.K_RIGHT and block.can_move_right:
-            block.dx = VELX
-        print 'block.dx', block.dx
-
-    if event.type == pygame.KEYUP:
-        if event.key == pygame.K_LEFT and block.dx < 0:
-            block.dx = 0
-        if event.key == pygame.K_RIGHT and block.dx > 0:
-            block.dx = 0
     return block
+
+
+def events(block):
+    keys = pg.key.get_pressed()
+
+    if not block.stop:
+        if keys[pg.K_LEFT] and block.can_move_left:
+            block.vel.x += -VELX
+        if keys[pg.K_RIGHT] and block.can_move_right:
+            block.vel.x += VELX
+        if keys[pg.K_DOWN]:
+            block.vel.y += VELY
+        if keys[pg.K_SPACE]:
+            block
+
+    return block
+
+
+############################################################
 
 
 ############################################################
@@ -141,16 +102,16 @@ def quit():
 
 def handle_common_events(event, running, cmd_key_down):
     # check for closing window
-    if event.type == pygame.QUIT:
+    if event.type == pg.QUIT:
         running = quit()
 
-    if event.type == pygame.KEYDOWN:
+    if event.type == pg.KEYDOWN:
         if event.key == 310:
             cmd_key_down = True
-        if cmd_key_down and event.key == pygame.K_q:
+        if cmd_key_down and event.key == pg.K_q:
             running = quit()
 
-    if event.type == pygame.KEYUP:
+    if event.type == pg.KEYUP:
         if event.key == 310:
             cmd_key_down = False
 
@@ -158,25 +119,21 @@ def handle_common_events(event, running, cmd_key_down):
 
 
 def run():
-    pygame.init()
-    pygame.mixer.init()  # for sound
-    screen = pygame.display.set_mode((WIDTH * GAME_UNIT, HEIGHT * GAME_UNIT))
+    pg.init()
+    pg.mixer.init()  # for sound
+    screen = pg.display.set_mode((WIDTH * GAME_UNIT, HEIGHT * GAME_UNIT))
     screen.fill(BLACK)
-    pygame.display.set_caption(GAME_NAME)
+    pg.display.set_caption(GAME_NAME)
 
-    clock = pygame.time.Clock()
+    clock = pg.time.Clock()
 
-    all_sprites = pygame.sprite.Group()
-    margins_with_blocks = pygame.sprite.Group()
+    all_sprites = pg.sprite.Group()
+    margins_with_blocks = pg.sprite.Group()
+    stoped_blocks = pg.sprite.Group()
 
-    block, sides, side_left, side_right, side_bottom = create_new_block()
+    # block, sides, side_left, side_right, side_bottom = create_new_block()
+    block = create_new_block(all_sprites)
 
-    # floor = pygame.sprite.Sprite()
-    # floor.image = pygame.Surface((WIDTH * GAME_UNIT, GAME_UNIT))
-    # floor.image.fill(BLUE)
-    # floor.rect = floor.image.get_rect()
-    # floor.rect.x = 0
-    # floor.rect.bottom = HEIGHT
     floor, wall_left, wall_right = create_margins()
     margins_with_blocks.add(floor)
     margins_with_blocks.add(wall_left)
@@ -185,67 +142,81 @@ def run():
     all_sprites.add(floor)
     all_sprites.add(wall_left)
     all_sprites.add(wall_right)
-    all_sprites.add(block)
 
     cmd_key_down = False
 
     running = True
     while running:
         clock.tick(FPS)
+        now = pg.time.get_ticks()
+        if block.stop:
+            print 'stop'
+            block = create_new_block(all_sprites)
+
+        block.vel = vec(0, 0)
 
         # Process input (events)
-        for event in pygame.event.get():
+        for event in pg.event.get():
             running, cmd_key_down = \
                 handle_common_events(event, running, cmd_key_down)
-            block = events(event, block)
+
+        if now - block.time_to_press > TIME_TO_PRESS:
+            block.time_to_press = now
+            block = events(block)
 
         # Update
-        now = pygame.time.get_ticks()
-        # hits = pygame.sprite.groupcollide(sides, margins_with_blocks,
-        #                                   False, False)
-        # print hits
+        all_sprites.update()
+        if now - block.update_time > TIME_TO_MOVE:
+            block.update_time = now
+            if not block.stop:
+                block.pos.y += VELY
 
-        hit_left = pygame.sprite.spritecollide(side_left, margins_with_blocks,
-                                               False)
-        # print hit_left
-        if hit_left:
+        block.pos += block.vel
+
+        # collisions:
+
+        def stop_the_block(block, stoped_blocks):
+            block.stop = True
+            stoped_blocks.add(block)
+            return block, stoped_blocks
+
+        # collstion with bottom:
+        # collision with other blocks:
+        block.rect.y += 1
+        hits = pg.sprite.spritecollide(block, stoped_blocks, False)
+        if hits:
+            block, stoped_blocks = stop_the_block(block, stoped_blocks)
+        if pg.sprite.collide_rect(block, floor):
+            block, stoped_blocks = stop_the_block(block, stoped_blocks)
+        block.rect.y -= 1
+        # collstion with left:
+        block.rect.x -= 1
+        hits = pg.sprite.spritecollide(block, stoped_blocks, False) or\
+            pg.sprite.collide_rect(block, wall_left)
+        if hits:
             block.can_move_left = False
         else:
             block.can_move_left = True
-
-        hit_right = pygame.sprite.spritecollide(
-            side_right, margins_with_blocks, False)
-        print hit_right
-        if hit_right:
+        block.rect.x += 1
+        # collstion with right:
+        block.rect.x += 1
+        hits = pg.sprite.spritecollide(block, stoped_blocks, False) or\
+            pg.sprite.collide_rect(block, wall_right)
+        if hits:
             block.can_move_right = False
         else:
             block.can_move_right = True
+        block.rect.x -= 1
 
-        for unit in block:
-            unit.rect.x += block.dx
-        block.x += block.dx
-        # if hits:
-        #     for unit in block:
-        #         unit.rect.x = unit.oldx
-        for unit in block:
-            unit.oldx = unit.rect.x
-
-        if now - block.update_time > TIME_TO_MOVE:
-            block.update_time = now
-            for unit in block:
-                #         if not hits:
-                unit.rect.x += unit.dx
-                unit.rect.y += unit.dy
-
-        all_sprites.update()
+        block.rect.topleft = block.pos
 
         # Render (draw)
         screen.fill(BLACK)
         all_sprites.draw(screen)
 
-        pygame.display.flip()  # always the last
+        pg.display.flip()  # always the last
 
-    pygame.quit()
+    pg.quit()
 
 
 run()
